38e9ecac8f5beec2b483dec5e690823156a44c53
Jake Choi <9822cj1@gmail.com>
2022-11-08 10:25:24 -0800

    add do while loop

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,15 @@
 //     i = i*2;
 // } while(i <= 1024);
 // console.log(i)
+
+let input = "";
+let numInput = "";
+do {
+    input = prompt("What is your name ");
+    // console.log(Number(input));
+}
+while(
+    // !isNaN(input)
+    Number(input) || input == 0
+);
+console.log(input);
