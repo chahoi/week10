468e87f2e8ceca5d96e20a6fc22bd45b39d5e976
Jake Choi <9822cj1@gmail.com>
2022-11-08 11:00:46 -0800

    objects

--- scripts/script.js
+++ scripts/script.js
@@ -14,18 +14,54 @@
 // } while(i <= 1024);
 // console.log(i)
 
-let input = "";
-let numInput = "";
-do {
-    input = prompt("What is your name ");
-    // console.log(Number(input));
-}
-while(
-    !isNaN(input)
-    // Number(input) || input == 0
-);
-console.log(input);
+// let input = "";
+// let numInput = "";
+// do {
+//     input = prompt("What is your name ");
+//     // console.log(Number(input));
+// }
+// while(
+//     !isNaN(input)
+//     // Number(input) || input == 0
+// );
+// console.log(input);
 
-//Template literal.
-// alert("Welcome " + input);
-alert(`welcome ${input}`);
+// //Template literal.
+// // alert("Welcome " + input);
+// alert(`welcome ${input}`);
+
+let fruits = ["Apple", "Bananna"];
+
+//add to last element
+fruits.push("Orange")
+console.log(fruits)
+
+//remove last element
+fruits.pop()
+console.log(fruits)
+
+//add to first element
+fruits.unshift("Mango")
+console.log(fruits)
+
+//remove first element of array
+fruits.shift(fruits)
+console.log(fruits)
+
+for (let item of fruits)
+    {
+        console(item);
+    }
+
+    const person -{
+        name: ["bob", "smith"],
+        age: 32,
+        gemder: 'male',
+        interests: ["music", "skiing"],
+    }
+// console.log(`My name is ${person.name[0]} . ${person.interest[0]}`
+
+for (let prop in person)
+{
+    console.log(`my name is ${prop} is ${person[prop]}`)
+}
