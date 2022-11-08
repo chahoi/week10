92cc23881d2a94db4fa77e970569d3fc707b5ca5
Jake Choi <9822cj1@gmail.com>
2022-11-08 09:54:13 -0800

    for loop

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// alert("The rest of the page wouldn't load until you reeeeeeeeeee");
+
+// for (let i =2; i<=1024; i = i*2) {
+//     console.log(i)
+// }
+
+// while (i <= 1024 i) {
+//     console.log(i);
+// }
+
+// do {
+//     i = 2;  
+//     i = i*2;
+// } while(i <= 1024);
+// console.log(i)
